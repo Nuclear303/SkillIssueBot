f365db20356d2069e04039104499cb7bdce7465e
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-01-09 20:12:30 +0100

    Added embed to the command

--- commands/messageMods.js
+++ commands/messageMods.js
@@ -1,9 +1,16 @@
-const { SlashCommandBuilder, SlashCommandUserOption } = require("@discordjs/builders")
+const { SlashCommandBuilder, SlashCommandUserOption } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
   .setName('message').setDescription("Messages bot-output channel"),
   async execute(interaction){
-    await interaction.guild.channels.cache.get("1062081528567431218").send("Hello!");
+    const messageEmbed = new EmbedBuilder()
+      .setColor(0xFF0000)
+      .setTitle("Test embed")
+      .addFields({name: "Hello message", value:"Hello guys! I'm a bot!"})
+      .setTimestamp()
+      .setFooter({text:"example embed"})
+    await interaction.guild.channels.cache.get("1062081528567431218").send({embeds:[messageEmbed]});
   }
 }
