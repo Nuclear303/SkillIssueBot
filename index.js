6d9bb24fe239512c79d2ee1e273910c778ebafb6
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-01-09 20:34:28 +0100

    Added dming kicked people

--- index.js
+++ index.js
@@ -70,8 +70,19 @@ client.on('guildMemberAdd', member => {
       {name: "ID:", value:`${member.id}`})
       .setImage(`${member.displayAvatarURL()}`)
       .setTimestamp()
-      .setFooter({text:"Skill Issue Bot"})
+      .setFooter({text:"Skill Issue Bot"});
+
+      
+    const dmEmbed = new EmbedBuilder()
+      .setColor(0xFF0000)
+      .setTitle("You got kicked")
+      .addFields({name: "Reason:", value:"Default profile picture (Safety measure)"},
+      {name: "What can I do?", value:"Please change your profile picture and you'll be more than welcome to join!"},
+      {name:"Why do you do that?", value:"It's a bot prevention mechanism"})
+      .setTimestamp()
+      .setFooter({text:"Skill Issue Bot"});
     member.guild.channels.cache.get("1062081528567431218").send({embeds:[messageEmbed]});
+    member.send({embeds:[dmEmbed]});
     member.kick();
   }
 })
