547680252fde56090a4335314e652271ef864169
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-01-09 19:55:32 +0100

    Added messaging command

---
+++ commands/messageMods.js
@@ -0,0 +1,9 @@
+const { SlashCommandBuilder, SlashCommandUserOption } = require("@discordjs/builders")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName('message').setDescription("Messages botOutput channel"),
+  async execute(interaction){
+    await interaction.guild.channels.cache.get("1062081528567431218").send("Hello!");
+  }
+}
