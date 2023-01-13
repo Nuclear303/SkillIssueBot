9e75551b575767de7e3a45621de2114aa3941b40
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-01-13 15:16:10 +0100

    Exported universal embeds to another file

---
+++ embeds/embeds.js
@@ -0,0 +1,9 @@
+
+export const KickDMEmbed = new EmbedBuilder()
+.setColor(0xFF0000)
+.setTitle("You got kicked")
+.addFields({name: "Reason:", value:"Default profile picture (Safety measure)"},
+  {name: "What can I do?", value:"Please change your profile picture and you'll be more than welcome to join!"},
+  {name:"Why do you do that?", value:"It's a bot prevention mechanism"})
+.setTimestamp()
+.setFooter({text:"Skill Issue Bot"});
