5bb65a65661755049ccb54d445c21392e2473e72
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-01-14 20:38:00 +0100

    Try

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ client.on("guildBanAdd", (member) => {
   .setFooter({text:"Skill Issue Bot - Member Banned"})
   .setTimestamp()
   
-  member.guild.bans.fetch(`${member.user.id}`).then(ban =>{banMember.addFields({name:"Reason", value:`${ban.reason}`})})
+  member.guild.bans.fetch(`${member.user.id}`).then(ban =>{console.log(ban.reason);banMember.addFields({name:"Reason", value:`${ban.reason}`})})
   client.channels.fetch("999028671895584848").then(channel =>{
     channel.send({embeds:[banMember]});
   })
