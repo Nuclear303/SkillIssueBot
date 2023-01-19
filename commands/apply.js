776e80f81fbd97a539ce054fc289505b102696ac
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-01-19 11:46:49 +0100

    Added /apply command, work in progress

--- commands/apply.js
+++ commands/apply.js
@@ -4,24 +4,23 @@ module.exports = {
   data: new SlashCommandBuilder()
   .setName('apply')
   .setDescription("Sends an application to one of our squadrons")
-  // .addStringOption((option)=>{
-  //   option
-  //   .setName('ign')
-  //   .setDescription('Your in-game nickname')
-  //   .setRequired(true)
-  // })
-  // .addStringOption((option)=>{
-  //   option
-  //   .setName('squadron')
-  //   .setDescription('Tag of the squadron you are applying to (Twix/Marz/Mlky/BNTY)')
-  //   .setRequired(true)
-  //   .addChoices(
-  //     {name:"Twix", value:"twix"},
-  //     {name:"Marz", value:"marz"},
-  //     {name:"Mlky", value:"mlky"},
-  //     {name:"BNTY", value:"bnty"},
-  //   )
-  // })
+  .addStringOption(option =>{
+    option.setName('ign')
+    .setDescription('Your in-game nickname')
+    .setRequired(true)
+  })
+  .addStringOption(option =>{
+    option
+    .setName('squadron')
+    .setDescription('Tag of the squadron you are applying to (Twix/Marz/Mlky/BNTY)')
+    .setRequired(true)
+    .addChoices(
+      {name:"Twix", value:"twix"},
+      {name:"Marz", value:"marz"},
+      {name:"Mlky", value:"mlky"},
+      {name:"BNTY", value:"bnty"},
+    )
+  })
   ,
   async execute(interaction){
     await interaction.reply('Test');
