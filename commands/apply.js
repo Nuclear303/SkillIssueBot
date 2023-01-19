84171b476e9d1af02f97ee89804e1d3667df1566
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-01-19 11:42:10 +0100

    Added /apply command, work in progress

--- commands/apply.js
+++ commands/apply.js
@@ -6,7 +6,7 @@ module.exports = {
   .setDescription("Sends an application to one of our squadrons")
   .addStringOption((option)=>{
     option
-    .setName("ign")
+    .setName('ign')
     .setDescription('Your in-game nickname')
     .setRequired(true)
   })
@@ -15,6 +15,12 @@ module.exports = {
     .setName('squadron')
     .setDescription('Tag of the squadron you are applying to (Twix/Marz/Mlky/BNTY)')
     .setRequired(true)
+    .addChoices(
+      {name:"Twix", value:"twix"},
+      {name:"Marz", value:"marz"},
+      {name:"Mlky", value:"mlky"},
+      {name:"BNTY", value:"bnty"},
+    )
   }),
   async execute(interaction){
     await interaction.reply('Test');
