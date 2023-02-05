8285f5c066e1432f6ea5a037b95bf8290f596505
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-02-05 10:27:22 +0100

    Automatic adding @* role to every person who doesn't have it

--- commands/add.js
+++ commands/add.js
@@ -4,12 +4,10 @@ module.exports = {
   data: new SlashCommandBuilder()
   .setName('add').setDescription("adds star"),
   async execute(interaction){
-    interaction.guilds.fetch("735871800730189916", false).then(guild=>{
       interaction.guild.members.cache.every(member=>{
         if(!member.roles.cache.has("1051078951885357108")){
           member.roles.add("1051078951885357108");
         }
       })
-    })
   }
 }
