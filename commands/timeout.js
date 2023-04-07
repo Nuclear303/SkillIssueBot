6926b5ad87cf601f0c57b4f39440c76f3be53f19
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-04-07 16:07:42 +0200

    timeout

--- commands/timeout.js
+++ commands/timeout.js
@@ -19,5 +19,6 @@ module.exports = {
     const time = interaction.options.getNumber('time');
     const member = interaction.guild.members.cache.get(target.id);
     member.timeout(1000*60*time, "Timeouted by moderation");
+    await interaction.reply({content:`Successfully timeouted ${target.username}`, ephemeral:true});
   }
 }
