b644937686e26a64ecb4a2631c4b0870ea3c9b80
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-10-21 23:07:10 +0200

    Fixed selfharm filter

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ client.on("messageCreate", message =>{
         message.delete();
       }).catch(_=>{});
     }
-    else if(mess.includes(" kys ") || mess.includes("kуs") || mess.includes("куs")|| mess.includes("кys") ||(mess.includes("kill") && (mess.includes("yourself") || mess.includes("your self") || mess.includes("yourselves") || mess.includes("your selves")))){
+    else if(mess.includes("kys ") || mess == "kys" || mess.includes("kуs") || mess.includes("куs")|| mess.includes("кys") ||(mess.includes("kill") && (mess.includes("yourself") || mess.includes("your self") || mess.includes("yourselves") || mess.includes("your selves")))){
       message.member.timeout(1000*600, "Encouraging selfharm").catch(_=>{});
       message.member.user.send({embeds: [selfharmEmbed]}).then(_=>{
         message.delete();
