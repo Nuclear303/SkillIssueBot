ece1b45599d414d1234461bb19fd28e1bcf6550a
Nuclear303 <oliwier.kotlowski1@gmail.com>
2024-09-25 19:30:50 +0200

    quickfix to closing ticket when the member is no longer on the server

--- commands/closeTicket.js
+++ commands/closeTicket.js
@@ -9,29 +9,36 @@ module.exports = {
       await interaction.reply({content:"Can't close ticket. Not a ticket channel", ephemeral:true});
       return
     }
-    interaction.channel.permissionOverwrites.set([{
-      id: interaction.channel.name.split("-")[1],
-      allow:[PermissionFlagsBits.ViewChannel],
-      deny: [PermissionFlagsBits.SendMessages]
-    },
-    {
-      id:"1048606041597812798",
-      allow:[PermissionFlagsBits.ViewChannel]
-    },
-    {
-      id:"998682580268367884",
-      allow:[PermissionFlagsBits.ViewChannel]
-    },
-    {
-      id:"893305484537397278",
-      allow:[PermissionFlagsBits.ViewChannel]
-    },
-    {
-      id: "894458473247567882",
-      deny:[PermissionFlagsBits.ViewChannel]
+    if(interaction.guild.members.cache.get(interaction.channel.name.split("-")[1])){
+      interaction.channel.permissionOverwrites.set([{
+        id: interaction.channel.name.split("-")[1],
+        allow:[PermissionFlagsBits.ViewChannel],
+        deny: [PermissionFlagsBits.SendMessages]
+      },
+      {
+        id:"1048606041597812798",
+        allow:[PermissionFlagsBits.ViewChannel]
+      },
+      {
+        id:"998682580268367884",
+        allow:[PermissionFlagsBits.ViewChannel]
+      },
+      {
+        id:"893305484537397278",
+        allow:[PermissionFlagsBits.ViewChannel]
+      },
+      {
+        id: "894458473247567882",
+        deny:[PermissionFlagsBits.ViewChannel]
+      }
+      ])
+        interaction.channel.setParent("1202957108987830282");
+        await interaction.reply("Ticket closed.")
+      }
+      else{
+        interaction.channel.setParent("1202957108987830282");
+        await interaction.reply("Ticket closed. Member no longer on the server")
+      }
     }
-    ])
-    interaction.channel.setParent("1202957108987830282");
-    await interaction.reply("Ticket closed.")
-  }
+    
 }
